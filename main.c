db63411072264b171cc065fbfbea6e7c5f8cfc45
Nick_F21 <93729322+WaffleMonster21@users.noreply.github.com>
2024-05-31 12:37:09 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2024 STMicroelectronics.
+  * Copyright (c) 2023 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -21,13 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "CO_app_STM32.h"						//Required for CANopen
-#include "CANopen.h"							//Required for CANopen
-#include "../CANopenNode/301/CO_SDOclient.h"	//Required for CANopen
-#include <string.h>
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -46,157 +40,27 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-CAN_HandleTypeDef hcan2;
-
-DAC_HandleTypeDef hdac;
-
-TIM_HandleTypeDef htim3;
-TIM_HandleTypeDef htim10;
-TIM_HandleTypeDef htim14;
+RTC_HandleTypeDef hrtc;
 
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-			//Global variable for transmitted variable
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_TIM3_Init(void);
-static void MX_TIM10_Init(void);
-static void MX_DAC_Init(void);
-static void MX_CAN2_Init(void);
-static void MX_TIM14_Init(void);
+static void MX_RTC_Init(void);
 /* USER CODE BEGIN PFP */
-double calcTORQUE(double network_freq, double motor_speed, double target_speed);
-uint32_t calcCANTrefvalue(double torque_ref,double torque_rated);
 
-/*CANopen function definitions -----------------------------------------------*/
-CO_SDO_abortCode_t read_SDO(CO_SDOclient_t *SDO_C, uint8_t nodeId,
-                            uint16_t index, uint8_t subIndex,
-                            uint8_t *buf, size_t bufSize, size_t *readSize)
-{
-    CO_SDO_return_t SDO_ret;
-
-    // setup client (this can be skipped, if remote device don't change)
-    SDO_ret = CO_SDOclient_setup(SDO_C,
-                                 CO_CAN_ID_SDO_CLI + nodeId,
-                                 CO_CAN_ID_SDO_SRV + nodeId,
-                                 nodeId);
-    if (SDO_ret != CO_SDO_RT_ok_communicationEnd) {
-        return CO_SDO_AB_GENERAL;
-    }
-
-    // initiate upload
-    SDO_ret = CO_SDOclientUploadInitiate(SDO_C, index, subIndex, 1000, false);
-    if (SDO_ret != CO_SDO_RT_ok_communicationEnd) {
-        return CO_SDO_AB_GENERAL;
-    }
-
-    // upload data
-    do {
-        uint32_t timeDifference_us = 10000;
-        CO_SDO_abortCode_t abortCode = CO_SDO_AB_NONE;
-
-        SDO_ret = CO_SDOclientUpload(SDO_C,
-                                     timeDifference_us,
-                                     false,
-                                     &abortCode,
-                                     NULL, NULL, NULL);
-        if (SDO_ret < 0) {
-            return abortCode;
-        }
-
-        HAL_Delay(1);
-    } while(SDO_ret > 0);
-
-    // copy data to the user buffer (for long data function must be called
-    // several times inside the loop)
-    *readSize = CO_SDOclientUploadBufRead(SDO_C, buf, bufSize);
-
-    return CO_SDO_AB_NONE;
-}
-
-CO_SDO_abortCode_t write_SDO(CO_SDOclient_t *SDO_C, uint8_t nodeId,
-                             uint16_t index, uint8_t subIndex,
-                             uint8_t *data, size_t dataSize)
-{
-    CO_SDO_return_t SDO_ret;
-    bool_t bufferPartial = false;
-
-    // setup client (this can be skipped, if remote device is the same)
-    SDO_ret = CO_SDOclient_setup(SDO_C,
-                                 CO_CAN_ID_SDO_CLI + nodeId,
-                                 CO_CAN_ID_SDO_SRV + nodeId,
-                                 nodeId);
-    if (SDO_ret != CO_SDO_RT_ok_communicationEnd) {
-        return -1;
-    }
-
-    // initiate download
-    SDO_ret = CO_SDOclientDownloadInitiate(SDO_C, index, subIndex,
-                                           dataSize, 1000, false);
-    if (SDO_ret != CO_SDO_RT_ok_communicationEnd) {
-        return -1;
-    }
-
-    // fill data
-    size_t nWritten = CO_SDOclientDownloadBufWrite(SDO_C, data, dataSize);
-    if (nWritten < dataSize) {
-        bufferPartial = true;
-        // If SDO Fifo buffer is too small, data can be refilled in the loop.
-    }
-
-    //download data
-    do {
-        uint32_t timeDifference_us = 10000;
-        CO_SDO_abortCode_t abortCode = CO_SDO_AB_NONE;
-
-        SDO_ret = CO_SDOclientDownload(SDO_C,
-                                       timeDifference_us,
-                                       false,
-                                       bufferPartial,
-                                       &abortCode,
-                                       NULL, NULL);
-        if (SDO_ret < 0) {
-            return abortCode;
-        }
-
-        HAL_Delay(1);
-    } while(SDO_ret > 0);
-
-    return CO_SDO_AB_NONE;
-}
-
-/*End of CANopen function definition -----------------------------------------*/
-
-
+RTC_HandleTypeDef RtcHandle;
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-#define Sys_Freq 84000000
-#define timer3Clock 1000000
-#define INPUT_BUFF_SIZE 512
-
-
-int startFlag = 0;
-uint8_t txbuf[64];
-
-void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
-	if (htim == &htim3){
-
-		if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1){
-
-			startFlag = 1;
-	}
-	}
-}
-
-
 
 /* USER CODE END 0 */
 
@@ -207,23 +71,7 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim){
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	uint16_t firstEdgeFlag = 0;
-	double TimValue1 = 0;
-	double TimValue2 = 0;
-	double Difference = 0;
-	double Freq = 50.0;
-	double Freq_min = 45.0;
-	double Freq_max = 51.0;
-	double Freq_normal  = 50.0;
 
-
-	double rated_speed = 3000; //motor constant
-	double motor_speed = 2500*0.1047197551;  //will be read by ADC from motor speed measurement
-	double target_speed = 3000*0.1047197551; //will be set by PC / Potentiometer (equivalent to thermostat)
-	double torque_ref = 8.30; //calculated by calcTORQUE function
-	double torque_rated = 13.40; //motor constant
-	uint32_t dac_value = 0; //calculated by calcDACvalue function
-	uint32_t Data_Tx = 0;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -245,32 +93,21 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
-  MX_TIM3_Init();
-  MX_TIM10_Init();
-  MX_DAC_Init();
-  MX_CAN2_Init();
-  MX_TIM14_Init();
+  MX_RTC_Init();
   /* USER CODE BEGIN 2 */
 
+  RTC_DateTypeDef sDate;
+  RTC_TimeTypeDef sTime;
 
-  //Code required for CANopen setup
-    CANopenNodeSTM32 canopenNodeSTM32;
-    canopenNodeSTM32.CANHandle = &hcan2;				//CAN module used in the MCU
-    canopenNodeSTM32.HWInitFunction = MX_CAN2_Init;
-    canopenNodeSTM32.timerHandle = &htim14;			//TIM14 selected as the interrupt timer
-    canopenNodeSTM32.desiredNodeID = 32;				//The node address of the micro-controller
-    canopenNodeSTM32.baudrate = 125;					//Desired baud rate for transmission
-    canopen_app_init(&canopenNodeSTM32);
+  sTime.Hours = 23;
+  sTime.Minutes = 59;
+  sTime.Seconds = 45;
+  HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
 
-    //Declaration of local variables
-    uint32_t amount_Read = 0;
+  char msg[64];
+  double freq = 50.123;
+  int counter = 0;
 
-  HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_1);
-  TIM4->CCR2 = 80;
-
-
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9,GPIO_PIN_SET);
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8,GPIO_PIN_RESET);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -280,48 +117,38 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  switch(startFlag){
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 
-	  case 1:
-		  if (firstEdgeFlag == 0){
-			  //sets the flag and measures the the inital value
-			  firstEdgeFlag = 1;
-			  TimValue1 = HAL_TIM_ReadCapturedValue(&htim3,TIM_CHANNEL_1);
-			  startFlag = 0;
+	  if (counter == 0){
+		  freq = 40;
+	  }
+	  else if (counter == 1){
+		  freq = 50;
+	  }
+	  else if (counter == 2){
+	  		  freq = 60;
+	  }
+	  else if (counter == 3){
+	  		  freq = 70;
+	  }
+	  else if (counter == 4){
+	  		  freq = 80;
+	  }
 
-		  }else{
-			  TimValue2 = HAL_TIM_ReadCapturedValue(&htim3,TIM_CHANNEL_1);
-			  if (TimValue2 > TimValue1){
-				  Difference = TimValue2 - TimValue1;
-			  }else{
-				  Difference = (0xffffffff - TimValue1) + TimValue2;
-			  }
+//	  HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BIN);
+//	  HAL_RTC_GetDate(&hrtc, &sDate, RTC_FORMAT_BIN);
 
+//	  sprintf(s, "%02d:%02d", sTime.Minutes, sTime.Seconds);
 
-			  Freq = timer3Clock / Difference;
-			  if (abs(Freq) < Freq_max && abs(Freq) > Freq_min){
-				  double tx_msg = Freq;
-				  sprintf((char*)txbuf, "Returned: %.3f \r\n", tx_msg );
-				  HAL_UART_Transmit(&huart2, txbuf, strlen((char*)txbuf), HAL_MAX_DELAY);
-				  __HAL_TIM_SET_COUNTER(&htim3, 0);
-	  				  //calculation of the new Tref and the assignment of that value to the new PWM value
-				  torque_ref = calcTORQUE(Freq, motor_speed, target_speed); //calculate Torque reference value in Nm
-				  // send over CAN. 8ms Delay
-				  Data_Tx = calcCANTrefvalue(torque_ref, torque_rated);
-				  write_SDO(canopenNodeSTM32.canOpenStack->SDOclient, 0x01, 0x2004, 0x08, &Data_Tx, 4);
-				  canopen_app_process();
-				  firstEdgeFlag = 0;
+	  sprintf((char*)msg, "%.3f\n", freq);
+	  HAL_UART_Transmit(&huart2, msg, strlen((char*)msg), HAL_MAX_DELAY);
+	  HAL_Delay(200);
 
-			  }else{
-				  firstEdgeFlag = 0;
-				  sprintf((char*)txbuf, "Error: %f\r\n", Freq);
-				  HAL_UART_Transmit(&huart2, txbuf, strlen((char*)txbuf), HAL_MAX_DELAY);
-				  Freq = 0;
-	  			  }
-			  startFlag = 0;
-	  		  }
-
-
+	  if (counter == 4){
+		  counter = 0;
+	  }
+	  else{
+		  counter = counter + 1;
 	  }
   }
   /* USER CODE END 3 */
@@ -344,11 +171,14 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
   RCC_OscInitStruct.PLL.PLLN = 336;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
   RCC_OscInitStruct.PLL.PLLQ = 2;
@@ -374,189 +204,65 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief CAN2 Initialization Function
+  * @brief RTC Initialization Function
   * @param None
   * @retval None
   */
-static void MX_CAN2_Init(void)
+static void MX_RTC_Init(void)
 {
 
-  /* USER CODE BEGIN CAN2_Init 0 */
+  /* USER CODE BEGIN RTC_Init 0 */
 
-  /* USER CODE END CAN2_Init 0 */
+  /* USER CODE END RTC_Init 0 */
 
-  /* USER CODE BEGIN CAN2_Init 1 */
+  RTC_TimeTypeDef sTime = {0};
+  RTC_DateTypeDef sDate = {0};
 
-  /* USER CODE END CAN2_Init 1 */
-  hcan2.Instance = CAN2;
-  hcan2.Init.Prescaler = 21;
-  hcan2.Init.Mode = CAN_MODE_NORMAL;
-  hcan2.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan2.Init.TimeSeg1 = CAN_BS1_13TQ;
-  hcan2.Init.TimeSeg2 = CAN_BS2_2TQ;
-  hcan2.Init.TimeTriggeredMode = DISABLE;
-  hcan2.Init.AutoBusOff = DISABLE;
-  hcan2.Init.AutoWakeUp = DISABLE;
-  hcan2.Init.AutoRetransmission = ENABLE;
-  hcan2.Init.ReceiveFifoLocked = DISABLE;
-  hcan2.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN CAN2_Init 2 */
+  /* USER CODE BEGIN RTC_Init 1 */
 
-  /* USER CODE END CAN2_Init 2 */
+  /* USER CODE END RTC_Init 1 */
 
-}
-
-/**
-  * @brief DAC Initialization Function
-  * @param None
-  * @retval None
+  /** Initialize RTC Only
   */
-static void MX_DAC_Init(void)
-{
+  hrtc.Instance = RTC;
+  hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
+  hrtc.Init.AsynchPrediv = 127;
+  hrtc.Init.SynchPrediv = 255;
+  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+  if (HAL_RTC_Init(&hrtc) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-  /* USER CODE BEGIN DAC_Init 0 */
+  /* USER CODE BEGIN Check_RTC_BKUP */
 
-  /* USER CODE END DAC_Init 0 */
+  /* USER CODE END Check_RTC_BKUP */
 
-  DAC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN DAC_Init 1 */
-
-  /* USER CODE END DAC_Init 1 */
-
-  /** DAC Initialization
+  /** Initialize RTC and set the Time and Date
   */
-  hdac.Instance = DAC;
-  if (HAL_DAC_Init(&hdac) != HAL_OK)
+  sTime.Hours = 23;
+  sTime.Minutes = 59;
+  sTime.Seconds = 45;
+  sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+  sTime.StoreOperation = RTC_STOREOPERATION_RESET;
+  if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN) != HAL_OK)
   {
     Error_Handler();
   }
+  sDate.WeekDay = RTC_WEEKDAY_TUESDAY;
+  sDate.Month = RTC_MONTH_DECEMBER;
+  sDate.Date = 31;
+  sDate.Year = 0;
 
-  /** DAC channel OUT1 config
-  */
-  sConfig.DAC_Trigger = DAC_TRIGGER_NONE;
-  sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
-  if (HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_1) != HAL_OK)
+  if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BIN) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN DAC_Init 2 */
+  /* USER CODE BEGIN RTC_Init 2 */
 
-  /* USER CODE END DAC_Init 2 */
-
-}
-
-/**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM3_Init(void)
-{
-
-  /* USER CODE BEGIN TIM3_Init 0 */
-
-  /* USER CODE END TIM3_Init 0 */
-
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_IC_InitTypeDef sConfigIC = {0};
-
-  /* USER CODE BEGIN TIM3_Init 1 */
-
-  /* USER CODE END TIM3_Init 1 */
-  htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 84-1;
-  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 65535;
-  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_IC_Init(&htim3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
-  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
-  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
-  sConfigIC.ICFilter = 0;
-  if (HAL_TIM_IC_ConfigChannel(&htim3, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM3_Init 2 */
-
-  /* USER CODE END TIM3_Init 2 */
-
-}
-
-/**
-  * @brief TIM10 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM10_Init(void)
-{
-
-  /* USER CODE BEGIN TIM10_Init 0 */
-
-  /* USER CODE END TIM10_Init 0 */
-
-  /* USER CODE BEGIN TIM10_Init 1 */
-
-  /* USER CODE END TIM10_Init 1 */
-  htim10.Instance = TIM10;
-  htim10.Init.Prescaler = 0;
-  htim10.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim10.Init.Period = 65535;
-  htim10.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim10.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim10) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM10_Init 2 */
-
-  /* USER CODE END TIM10_Init 2 */
-
-}
-
-/**
-  * @brief TIM14 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM14_Init(void)
-{
-
-  /* USER CODE BEGIN TIM14_Init 0 */
-
-  /* USER CODE END TIM14_Init 0 */
-
-  /* USER CODE BEGIN TIM14_Init 1 */
-
-  /* USER CODE END TIM14_Init 1 */
-  htim14.Instance = TIM14;
-  htim14.Init.Prescaler = 84 - 1;
-  htim14.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim14.Init.Period = 1000;
-  htim14.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim14.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim14) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM14_Init 2 */
-
-  /* USER CODE END TIM14_Init 2 */
+  /* USER CODE END RTC_Init 2 */
 
 }
 
@@ -611,7 +317,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LD2_Pin|GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -619,59 +328,26 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LD2_Pin PA8 PA9 */
-  GPIO_InitStruct.Pin = LD2_Pin|GPIO_PIN_8|GPIO_PIN_9;
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PB7 */
   GPIO_InitStruct.Pin = GPIO_PIN_7;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.Alternate = GPIO_AF2_TIM4;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
-double calcTORQUE(double network_freq, double motor_speed, double target_speed){
-	//initialize variables
-	float freq_1, freq_2 = 0; //freq_1 = network_freq for 49.75<fn<49.82, freq_2 = network_freq for 49.52<fn<49.75
-	float km = 0;
-	double fan_k = 0.128e-3;
-	double torque_ref_calc;
-	//set freq_1 and freq_2, if load shedding set torque reference to 0 Nm
-	if (network_freq <= 49.52){
-		torque_ref_calc = 0;
-	} else {
-		if (network_freq >= 49.75){
-			freq_1 = network_freq;
-			freq_2 = 49.75;
-		} else {
-			freq_1 = 49.75;
-			freq_2 = network_freq;
-		}
-		km= 1- ((49.82-freq_1)*8.571428571)- ((49.75-freq_2)*0.8695652174); //calculate km (percentage of initial motor power)
-		torque_ref_calc = km*((fan_k*target_speed*target_speed*target_speed)/motor_speed); //calculate torque reference in Nm
-	}
-	//return torque_ref in Nm;
-	return torque_ref_calc;
-}
-uint32_t calcCANTrefvalue(double torque_ref,double torque_rated){
 
-	double torque_percent = torque_ref / torque_rated; //calculate torque percetange value
-	uint32_t CAN_Tref = (uint32_t)(10000*torque_percent); //adapt to DAC output value
-	return CAN_Tref;
-}
 /* USER CODE END 4 */
 
 /**
